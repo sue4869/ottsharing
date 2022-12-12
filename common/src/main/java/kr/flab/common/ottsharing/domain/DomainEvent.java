d3f4bae8295f7bc5ad8bc15546d6abdb8b0e5737
sue4869 <sue4869@naver.com>
2022-12-12 13:29:55 +0900

    refactor : 오타 수정

--- common/src/main/java/kr/flab/common/ottsharing/domain/DomainEvent.java
+++ common/src/main/java/kr/flab/common/ottsharing/domain/DomainEvent.java
@@ -3,7 +3,7 @@ package kr.flab.common.ottsharing.domain;
 import java.time.Instant;
 
 public abstract class DomainEvent {
-    private final Instant occured = Instant.now();
+    private final Instant occurred = Instant.now();
 }
 
 
